f19ceec068cd7c92e98e19aaf560a8cfdc0de337
nestaenow <nestaenow@gmail.com>
2023-07-10 10:35:31 +0100

    MainScreen completed successfully

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,122 +1,148 @@
+import React, { useState, useEffect } from "react";
 import {
   KeyboardAvoidingView,
   Keyboard,
   StatusBar,
-  View,
-  TextInput,
   StyleSheet,
   TouchableWithoutFeedback,
-  TouchableOpacity,
-  Image,
+  Platform,
 } from "react-native";
-import React, { useState } from "react";
-import CustomButton from "../components/CustomButton";
+import * as Clipboard from "expo-clipboard";
+import Toast from "react-native-toast-message";
+import LanguageSelector from "../components/LanguageSelector";
+import TextInputComponent from "../components/TextInputComponent";
+import TranslatedOutput from "../components/TranslatedOutput";
 
 const MainScreen = () => {
-    const [inputText, setInputText] = useState("");
-    const [keyboardStatus, setKeyboardStatus] = useState(false);
+  const [inputText, setInputText] = useState("");
+  const [keyboardStatus, setKeyboardStatus] = useState(false);
+  const [translatedText, setTranslatedText] = useState("");
+  const [textInputMargin, setTextInputMargin] = useState(0);
+  const [textInputWidth, setTextInputWidth] = useState("100%");
+  const [textInputFlex, setTextInputFlex] = useState("1");
+  const [translationVisible, setTranslationVisible] = useState(false);
+  const [inputTagVisible, setInputTagVisible] = useState(false);
+  const [translationTagVisible, setTranslationTagVisible] = useState(false);
+  const [isTranslated, setIsTranslated] = useState(false);
 
-
-  const handlePress = () => {
+  const onPressLanguage1 = () => {
     console.log("Button pressed!");
+  };
+
+  const onPressLanguage2 = () => {
+    console.log("Button pressed!");
+  };
+
+  const handleFocus = () => {
+    setTextInputWidth("100%");
+    setTextInputFlex("1");
+    setInputTagVisible(false);
+    setIsTranslated(false);
   };
 
   const handleInputChange = (text) => {
     setInputText(text);
+    setTextInputWidth("100%");
+    setTextInputFlex("1");
+    setTranslationVisible(false);
+    setInputTagVisible(false);
+    setIsTranslated(false);
   };
 
-  const dismissKeyboard = () => {
-    Keyboard.dismiss();
-    };
-    
-    const handleTranslate = () => {
-      // handle the translation operation
-        console.log("Translate pressed!");
-    };
+  const handleTranslate = () => {
+    // Placeholder for translation function
+    const result = inputText; // replace with your own translation function
+    setTranslatedText(result);
+    setTextInputMargin(20); // adjust the TextInput margin
+    setTextInputWidth("90%");
+    setTextInputFlex("0");
+    setTranslationVisible(true);
+    setInputTagVisible(true);
+    setTranslationTagVisible(true);
+    Keyboard.dismiss(); // dismiss the keyboard
+    setIsTranslated(true);
+  };
 
   const clearInput = () => {
     setInputText("");
+  };
+
+  const handleCopy = () => {
+    Clipboard.setString(translatedText);
+    Toast.show({
+      type: "success",
+      position: "bottom",
+      text1: "Text Copied",
+      visibilityTime: 3000,
+      autoHide: true,
+      topOffset: 30,
+      bottomOffset: 40,
+      backgroundColor: "#FF9700",
+      textColor: "#FFF",
+      textStyle: {
+        textAlign: "center",
+      },
+      style: {
+        borderRadius: 20,
+      },
+    });
+  };
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      () => {
+        setKeyboardStatus(true);
+        setTranslationVisible(false);
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setKeyboardStatus(false);
+      }
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
     };
-    
-    React.useEffect(() => {
-      const keyboardDidShowListener = Keyboard.addListener(
-        "keyboardDidShow",
-        () => {
-          setKeyboardStatus(true);
-        }
-      );
-      const keyboardDidHideListener = Keyboard.addListener(
-        "keyboardDidHide",
-        () => {
-          setKeyboardStatus(false);
-        }
-      );
-
-      return () => {
-        keyboardDidHideListener.remove();
-        keyboardDidShowListener.remove();
-      };
-    }, []);
-
+  }, []);
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <KeyboardAvoidingView
         style={styles.container}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         keyboardVerticalOffset={Platform.OS === "ios" ? 0 : -500}
       >
         <StatusBar barStyle="dark-content" />
-        <View style={styles.languages}>
-          <CustomButton
-            textColor="#FFFFFF"
-            backgroundColor="#FF9700"
-            onPress={handlePress}
-            style={styles.language1}
-          >
-            English
-          </CustomButton>
-          <CustomButton
-            textColor="#FF9700"
-            backgroundColor="#FFF0DB"
-            onPress={handlePress}
-            style={styles.language2}
-          >
-            Kɛnyaŋ
-          </CustomButton>
-        </View>
-        <View style={styles.textInputContainer}>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Enter text"
-            value={inputText}
-            onChangeText={handleInputChange}
-            multiline
-            textColor="#5B5B5B"
-            onBlur={dismissKeyboard}
-          />
-          {inputText !== "" && (
-            <TouchableOpacity style={styles.clearButton} onPress={clearInput}>
-              <Image
-                style={styles.clearIcon}
-                source={require("../assets/clear.png")}
-              />
-            </TouchableOpacity>
-          )}
-          <TouchableOpacity
-            style={
-              keyboardStatus
-                ? styles.translateButtonKeyboardActive
-                : styles.translateButton
-            }
-            onPress={handleTranslate}
-          >
-            <Image
-              style={styles.translateIcon}
-              source={require("../assets/translate.png")}
-            />
-          </TouchableOpacity>
-        </View>
+        <LanguageSelector
+          onPressLanguage1={onPressLanguage1}
+          onPressLanguage2={onPressLanguage2}
+        />
+        <TextInputComponent
+          inputText={inputText}
+          textInputWidth={textInputWidth}
+          textInputFlex={textInputFlex}
+          inputTagVisible={inputTagVisible}
+          handleInputChange={handleInputChange}
+          handleFocus={handleFocus}
+          keyboardStatus={keyboardStatus}
+          clearInput={clearInput}
+          handleTranslate={handleTranslate}
+          isTranslated={isTranslated}
+        />
+        <TranslatedOutput
+          translatedText={translatedText}
+          handleCopy={handleCopy}
+          translationVisible={translationVisible}
+          textInputMargin={textInputMargin}
+          textInputWidth={textInputWidth}
+          textInputFlex={textInputFlex}
+          translationTagVisible={translationTagVisible}
+        />
+        <Toast ref={(ref) => Toast.setRef(ref)} />
       </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
   );
@@ -125,77 +151,8 @@ const MainScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 80,
+    marginTop: 60,
     alignItems: "center",
-  },
-  languages: {
-    flexDirection: "row",
-    marginBottom: 20,
-  },
-  language1: {
-    marginRight: 7.5,
-  },
-  language2: {
-    marginLeft: 7.5,
-  },
-  textInputContainer: {
-    flex: 1,
-    width: "100%",
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    paddingHorizontal: 40,
-    justifyContent: "start",
-    alignItems: "center",
-    backgroundColor: "#FFF7ED",
-  },
-  textInput: {
-    fontSize: 28,
-    fontWeight: "700",
-    backgroundColor: "#FFF7ED",
-    marginHorizontal: 40,
-    marginVertical: 40,
-    width: "100%",
-    borderRadius: 10,
-    color: "#5B5B5B",
-  },
-  clearButton: {
-    width: 14,
-    height: 14,
-    position: "absolute",
-    top: 20,
-    right: 40,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  clearIcon: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
-  },
-  translateButton: {
-    width: 80,
-    height: 80,
-    position: "absolute",
-    bottom: 50,
-    alignSelf: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 40,
-  },
-  translateButtonKeyboardActive: {
-    width: 46,
-    height: 46,
-    position: "absolute",
-    right: 24,
-    bottom: 20.74,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 23,
-  },
-  translateIcon: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
   },
 });
 
